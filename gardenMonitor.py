e52ee8f91460dc5ceee8c57a7e40f3241a895dbf
Sean Lantz <slantzjr@gmail.com>
2017-10-12 22:00:11 -0500

    Add pump control and modify script flow

--- gardenMonitor.py
+++ gardenMonitor.py
@@ -15,18 +15,32 @@ session.login()
 print 'Setting up GPIO'
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN)
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17,0)
 print 'GPIO setup complete'
 
-prevWater = True
+pumpOutput = 1
+
 while True:
-  temp = thermometer.read()
   hasWater = GPIO.input(26) == 0
+
+  # If the pipe is dry fill it up
+  while hasWater == False:
+    if pumpOutput == 1:
+      pumpOutput = 0
+      GPIO.output(17, pumpOutput)
+    hasWater = GPIO.input(26) == 0
+    time.sleep(10)
+
+  # Pipe should be full, turn pump off
+  pumpOutput = 1
+  GPIO.output(17, pumpOutput)
+ 
+  # Now we can take a measurement   
+  temp = thermometer.read()
   lightIntensity = lightMeasurement.readLight()  
   session.login()
   session.sendMeasurement(temp, hasWater, lightIntensity)
   session.logout()
   
-  if hasWater != prevWater:
-    gardenEmail.send_alert(hasWater)
-  prevWater = hasWater
   time.sleep(600)
